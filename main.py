60e7054e4a0240c1290bc03e6efc8b72d61c71fc
anna <141744002+xneqweg@users.noreply.github.com>
2024-12-16 12:14:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-number1 = float(input("Введите первое число: "))
-number2= float(input("Введите второе число: "))
-
-result = "Числа равны" if number1 == number2
-    else (f"Наименьшее число: {number1}" if number1 < number2 else f"Наименьшее число: {number2}")
-
-print(result)
+x = float(input("Введите первое число: "))
+y = float(input("Введите второе число: "))
+if x == y:
+    print("Числа равны") 
+else:
+    print("Первое число больше второго.") if x > y else print("Второе число больше первого.")
+   
